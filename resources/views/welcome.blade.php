072292c1b6522ef1251fafde2a6a439f93d92493
Your Name <you@example.com>
2024-08-26 15:24:28 -0300

    sus

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,18 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body {
+      background-color: #333;
+      color: #636b6f;
+      font-family: 'Nunito', sans-serif;
+      font-weight: 200;
+      height: 100vh;
+      margin: 0;
+    }
+  </style>
   @vite('resources/css/app.css')
+  
 </head>
 <body class="bg-gray-300">
   <h1 class="text-3xl font-bold underline">
