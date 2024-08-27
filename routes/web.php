84c3723b8c243a5162db94df00bbc76c12228c42
Your Name <you@example.com>
2024-08-27 05:24:40 -0300

    login

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/pinto', function (){
-  return view('./auth/login');
-})
 
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'registerForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
